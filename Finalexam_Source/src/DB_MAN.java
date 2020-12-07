69bf39727827431641280a0e98f96611ab359ce7
abruption <first_kuls@naver.com>
2020-12-07 14:29:22 +0900

    12.07 Final Exam Source Code

---
+++ Finalexam_Source/src/DB_MAN.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+import java.io.*;
+import java.util.*;
+
+public class DB_MAN {
+   String strDriver = "oracle.jdbc.driver.OracleDriver";                            // Oracle DB Driver
+   String strURL = "jdbc:oracle:thin:@localhost:1521";                              // Oracle DB Connection(localhost)
+   String strUser = "INHATC";                                                       // Oracle DB User ID
+   String strPWD = "inha1958";                                                      // Oracle DB Password
+    
+    Connection DB_con;      // DB Connection
+    Statement DB_stmt;      // To store statement for DB Connection
+    ResultSet DB_rs;        // To sotre result of SQL Excution
+    
+    public void dbOpen() throws IOException{
+        try{
+            Class.forName(strDriver);                       // Load JDBC-ODBC bridge driver
+            DB_con = DriverManager.getConnection(strURL, strUser, strPWD);   // Setting DSN Prooerties in driver
+            DB_stmt = DB_con.createStatement();             // To create statement for DB Connection
+        } catch (Exception e){
+            System.out.println("SQLException : " + e.getMessage());
+        }
+    }
+    
+    public void dbClose() throws IOException{
+        try{
+            DB_stmt.close();        // Exit Statement Connection
+            DB_con.close();         // Exit DB Connection
+        } catch (Exception e){
+            System.out.println("SQLexception : " + e.getMessage());
+        }
+    }
+}
